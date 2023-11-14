4e6940329d237a8439fa1d9f18b2c9895bc70217
LJkiller <OfficialLJkiller@gmail.com>
2023-11-14 01:59:53 +0100

    Modified some small HTML structure. Modified some JavaScript structure and making it easier to understand. Added a new feature: checking which video (ID) you're displaying.

--- Data/JavaScript/iframe-controller.js
+++ Data/JavaScript/iframe-controller.js
@@ -1,16 +1,29 @@
 const playerIframe = document.getElementById('falsifiedMediaPlayer');
 const widthInput = document.getElementById('widthInput');
+
 const heightValueSpan = document.getElementById('heightValue');
 const widthValueSpan = document.getElementById('widthValue');
+const videoIdValueSpan = document.getElementById('videoID');
 
 const baseWidth = 426;
 const baseHeight = 240;
 const scaleFactor = 3;
 
-let defaultWidth = baseWidth*scaleFactor;
-let defaultHeight = baseHeight*scaleFactor;
-let aspectRatio = defaultWidth/defaultHeight;
+let defaultWidth = baseWidth * scaleFactor;
+let defaultHeight = baseHeight * scaleFactor;
+let aspectRatio = defaultWidth / defaultHeight;
 
+
+//Saves the different values:
+function saveVideoDimensionValues(width = defaultWidth) {
+    localStorage.setItem('videoWidth', width);
+}
+function saveVideoIDValue(videoID = "NOT FOUND") {
+    localStorage.setItem('videoID', videoID);
+}
+
+
+//Updates old values to new values:
 function updateVideoSize() {
     const newWidth = parseInt(widthInput.value);
 
@@ -20,51 +33,44 @@ function updateVideoSize() {
 
     const newHeight = Math.round(newWidth / aspectRatio);
 
+    saveVideoDimensionValues(newWidth);
     updatePlayerDimensions(newWidth, newHeight);
-    saveVideoValue(playerIframe.width);
     displaySizeValues();
+
     widthInput.value = '';
 }
-
-function resetVideoSize() {
-    updatePlayerDimensions(defaultWidth, defaultHeight);
-    saveVideoValue();
-    displaySizeValues();
-}
-
 function updatePlayerDimensions(width, height) {
     playerIframe.width = width;
     playerIframe.height = height;
 }
-
-function saveVideoValue(width = defaultWidth) {
-    localStorage.setItem('videoWidth', width);
+function updateVideoID(videoID) {
+    videoIdValueSpan.textContent = `videoID: ${videoID}`;
 }
 
-function displaySizeValues() {
-    let storedWidth = localStorage.getItem('videoWidth');
-    let storedHeight = Math.round(storedWidth / aspectRatio);
 
-    heightValueSpan.textContent = `Height: ${storedHeight}px`;
-    widthValueSpan.textContent = `Width: ${storedWidth}px`;
+//Gets the link and then analyses the link and then sets as source:
+function extractAndSetVideoID() {
+    const linkInput = document.getElementById('linkInput').value;
+    const videoID = extractVideoId(linkInput);
+    updateVideoID(videoID);
+    return videoID;
 }
-
-displaySizeValues();
-
+//Extracts the id:
 function extractVideoId(link) {
     const watchPattern = /(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/(?:[^\/\n\s]+\/\S+\/|(?:v|e(?:mbed)?)\/|\S*?[?&]v=)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
     const shortPattern = /https?:\/\/youtu\.be\/([a-zA-Z0-9_-]{11})/;
     const watchMatch = link.match(watchPattern);
     const shortMatch = link.match(shortPattern);
-    const videoId = watchMatch ? watchMatch[1] : (shortMatch ? shortMatch[1] : null);
+    const videoID = watchMatch ? watchMatch[1] : (shortMatch ? shortMatch[1] : null);
 
-    return videoId;
+    return videoID;
 }
-
+//Sets the new ID as video/iframe src:
 function extractAndSetVideo(event) {
     event.preventDefault();
     const linkInput = document.getElementById('linkInput').value;
-    const iframeSrc = `https://www.youtube-nocookie.com/embed/${extractVideoId(linkInput)}?start=0&autoplay=1&autohide=1`;
+    const videoID = extractAndSetVideoID();
+    const iframeSrc = `https://www.youtube-nocookie.com/embed/${videoID}?start=0&autoplay=1&autohide=1`;
 
     if (iframeSrc) {
         console.log('Updated iframe src:', iframeSrc);
@@ -73,4 +79,32 @@ function extractAndSetVideo(event) {
     } else {
         console.error('Invalid YouTube link');
     }
+
+    saveVideoIDValue(videoID);
 }
+
+
+//Displays the new values:
+function displaySizeValues() {
+    let storedWidth = localStorage.getItem('videoWidth');
+    let storedHeight = Math.round(storedWidth / aspectRatio);
+
+    heightValueSpan.textContent = `Height: ${storedHeight}px`;
+    widthValueSpan.textContent = `Width: ${storedWidth}px`;
+}
+function displayVideoId() {
+    const storedVideoId = localStorage.getItem('videoID');
+    videoIdValueSpan.textContent = `videoID: ${storedVideoId}`;
+}
+
+
+//Resets displayValues, displays to default values.
+function resetVideoSize() {
+    updatePlayerDimensions(defaultWidth, defaultHeight);
+    saveVideoDimensionValues();
+    displaySizeValues();
+    displayVideoId();
+}
+
+displaySizeValues();
+displayVideoId();
